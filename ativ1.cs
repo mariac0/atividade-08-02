13b134c19ea651468539ce0de2baa3459f907339
Maria Carolina <111579435+mariac0@users.noreply.github.com>
2023-02-09 08:27:27 -0300

    Add files via upload

---
+++ ativ1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ativ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string um, dois, tres, quatro, cinco, seis, sete, oito, nove, dez;
+
+            Console.WriteLine("digite o primeiro nome");
+            um = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("digite o segundo nome");
+            dois = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("digite o terceiro nome");
+            tres = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("digite o quarto nome");
+            quatro = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("digite o quinto nome");
+            cinco = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("digite o sexto nome");
+            seis = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("digite o sétimo nome");
+            sete = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("digite o oitavo nome");
+            oito = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("digite o nono nome");
+            nove = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("digite o décimo nome");
+            dez = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("todos os nomes");
+            Console.WriteLine("");
+            Console.WriteLine(um);
+            Console.WriteLine(dois);
+            Console.WriteLine(tres);
+            Console.WriteLine(quatro);
+            Console.WriteLine(cinco);
+            Console.WriteLine(seis);
+            Console.WriteLine(sete);
+            Console.WriteLine(oito);
+            Console.WriteLine(nove);
+            Console.WriteLine(dez);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ativ2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ativ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double N1, N2, N3, N4, N5, soma, mediana, media;
+
+            Console.WriteLine("Escreva o primeiro numero");
+            N1 = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Escreva o segundo numero");
+            N2 = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Escreva o terceiro numero");
+            N3 = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Escreva o quarto numero");
+            N4 = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Escreva o quinto numero");
+            N5 = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            mediana = N3;
+
+            soma = N1 + N2 + N3 + N4 + N5;
+
+            media = N1 + N2 + N3 + N4 + N5;
+
+            media = media / 5;
+
+            Console.WriteLine("MÉDIA" + media);
+            Console.WriteLine("Mediana" + mediana);
+            Console.WriteLine("Soma" + soma);
+            Console.ReadKey();
+        }
+    }
+}
+ 
